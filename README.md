fc39f01282fc26326f0ce1864737aefcf72fc9ae
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2021-02-01 11:45:07 -0600

    rewrite new hello package using go modules tutorial

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Hello [![Build Status](https://travis-ci.org/whatsadebugger/hello.svg?branch=master)](https://travis-ci.org/whatsadebugger/hello) [![codecov](https://codecov.io/gh/whatsadebugger/hello/branch/master/graph/badge.svg)](https://codecov.io/gh/whatsadebugger/hello)
-
-# testing travis ci, codecov 
+# Hello
--- hello.go
+++ hello.go
@@ -2,70 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"strconv"
 
-	"github.com/urfave/cli"
+	"github.com/whatsadebugger/greetings"
 )
 
 func main() {
-
-	app := cli.NewApp()
-
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:  "file, f",
-			Usage: "file input path",
-		},
-	}
-	app.Commands = []cli.Command{
-		cli.Command{
-			Name:   "add",
-			Usage:  "add two arguments",
-			Action: add,
-		},
-		cli.Command{
-			Name:   "multiply",
-			Usage:  "multiply two arguments",
-			Action: multiply,
-		},
-	}
-
-	app.Action = dostuff
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func dostuff(c *cli.Context) error {
-	name := "someone"
-	if c.NArg() > 0 && c.IsSet("file") {
-		name = c.Args()[0]
-	}
-	fmt.Print(name, "\n")
-
-	return nil
-}
-
-func add(c *cli.Context) error {
-	args := c.Args()
-	a, _ := strconv.Atoi(args[0])
-
-	b, _ := strconv.Atoi(args[1])
-	fmt.Println(a + b)
-
-	return nil
-}
-
-func multiply(c *cli.Context) error {
-	args := c.Args()
-	a, _ := strconv.Atoi(args[0])
-
-	b, _ := strconv.Atoi(args[1])
-	fmt.Println(a * b)
-
-	return nil
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
